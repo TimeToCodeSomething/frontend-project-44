c2cbaf41fe4f1c6dbbaf424cf488b0e9c1733e81
TimeToCodeSomething <osel15278@gmail.com>
2024-05-27 12:02:33 +0700

    Modified

--- src/odd_or_even.js
+++ src/odd_or_even.js
@@ -1,5 +1,32 @@
 import readlineSync from "readline-sync";
 
+function start_round() {
+  return Math.floor(Math.random() * 999 + 1);
+}
+
+function get_answer(number) {
+  console.log("Question: " + number.toString());
+  return readlineSync.question("Your answer: ");
+}
+
+function process_answer(answer, correctAnswer, correctAnswers, name) {
+  if (correctAnswer == answer) {
+    console.log("Correct!");
+    correctAnswers++;
+  } else {
+    console.log(
+      '"' +
+        answer +
+        '"' +
+        " is wrong answer ;(. Correct answer was '" +
+        correctAnswer +
+        "'",
+    );
+    console.log("Let's try again, " + name + "!");
+  }
+  return correctAnswers;
+}
+
 export function odd_or_even(name) {
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
@@ -9,25 +36,18 @@ export function odd_or_even(name) {
   };
   var number = 0;
   var correctAnswers = 0;
+  var answer = "";
   for (var i = 0; i < 3; i++) {
-    number = Math.floor(Math.random() * 999 + 1);
-    console.log("Question: " + number.toString());
-    const answer = readlineSync.question("Your answer: ");
-    var correctAnswer = correct[(number % 2 == 0).toString()];
-
-    if (correctAnswer == answer) {
-      console.log("Correct!");
-      correctAnswers++;
-    } else {
-      console.log(
-        '"' +
-          answer +
-          '"' +
-          " is wrong answer ;(. Correct answer was '" +
-          correctAnswer +
-          "'",
-      );
-      console.log("Let's try again, " + name + "!");
+    number = start_round(number);
+    answer = get_answer(number);
+    var correctAnswer = correct[number % 2 == 0];
+    correctAnswers = process_answer(
+      answer,
+      correctAnswer,
+      correctAnswers,
+      name,
+    );
+    if (i == correctAnswers) {
       break;
     }
   }
