99f63628141f8861df672cf2df9b5e758e467c33
TimeToCodeSomething <osel15278@gmail.com>
2024-05-29 01:45:16 +0700

    ..

--- src/games/progression.js
+++ src/games/progression.js
@@ -19,20 +19,31 @@ function generate_message(progression) {
 }
 
 function prepare_question() {
-  
+  var progression = [];
+  var startNumber,
+    step,
+    correctAnswer,
+    progressionLength = 0;
 
+  startNumber = game_handler.generate_instance(1, 100);
+  progressionLength = game_handler.generate_instance(1, 20, 5);
+  step = game_handler.generate_instance(1, 50);
+  progression = generate_progession(
+    Number(startNumber),
+    Number(step),
+    progressionLength,
+  );
 
+  var indexToOmit = game_handler.generate_instance(1, progressionLength);
+  correctAnswer = progression[indexToOmit - 1];
+  progression[indexToOmit - 1] = "..";
+  var message = generate_message(progression);
 
   return [message, correctAnswer];
 }
 
 export function progession_game(name) {
   console.log("What number is missing in the progression?");
-  var progression = [];
-  var startNumber,
-    step,
-    correctAnswer,
-    progressionLength = 0;
 
   var answer = "";
   var correctAnswers = 0;
@@ -40,19 +51,9 @@ export function progession_game(name) {
   var correctAnswer = 0;
 
   for (var i = 0; i < 3; i++) {
-    startNumber = game_handler.generate_instance(1, 100);
-    progressionLength = game_handler.generate_instance(1, 20, 5);
-    step = game_handler.generate_instance(1, 50);
-    progression = generate_progession(
-      Number(startNumber),
-      Number(step),
-      progressionLength,
-    );
-  
-    var indexToOmit = game_handler.generate_instance(1, progressionLength);
-    correctAnswer = progression[indexToOmit - 1];
-    progression[indexToOmit - 1] = "..";
-    var message = generate_message(progression);
+    var data = prepare_question();
+    message = data[0];
+    correctAnswer = data[1];
 
     answer = game_handler.get_answer(message);
     correctAnswers = game_handler.process_answer(
