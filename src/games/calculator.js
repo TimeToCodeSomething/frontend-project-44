508b518f191717fbf60080637d12550c659bf22e
TimeToCodeSomething <osel15278@gmail.com>
2024-05-29 16:21:28 +0700

    calculator.js style fix

--- src/games/calculator.js
+++ src/games/calculator.js
@@ -1,38 +1,48 @@
-import * as game_handler from '../index.js';
+import * as gameHandler from '../index.js';
 
 function generateMessage(numbers, operation) {
-  var operations = {
+  let operations = {
     1: '+',
     2: '-',
     3: '*',
   };
-  var message = `${numbers[0]} ${operations[operation]} ${numbers[1]}`;
+  let message = `${numbers[0]} ${operations[operation]} ${numbers[1]}`;
 
   return message;
+}
+
+function calculateAnswer(numbers, operation){
+  if (operation[0] === 1) {
+    return Number(numbers[0]) + Number(numbers[1]);
+  } else if (operation[0] === 2) {
+    return Number(numbers[0]) - Number(numbers[1]);
+  } else if (operation[0] === 3) {
+    return Number(numbers[0]) * Number(numbers[1]);
+  }
 }
 
 export default function calculator(name) {
   console.log('What is the result of the expression?');
 
-  var numbers = [];
-  var operation = '';
-  var answer = '';
-  var correctAnswers = 0;
+  let numbers = [];
+  let operation = '';
+  let answer = '';
+  let correctAnswers = 0;
 
-  for (var i = 0; i < 3; i = i + 1) {
-    numbers = game_handler.generateInstance(2, 100);
-    operation = game_handler.generateInstance(1, 4);
-    var message = generateMessage(numbers, operation);
+  for (var i = 0; i < 3; i += 1) {
+    numbers = gameHandler.generateInstance(2, 100);
+    operation = gameHandler.generateInstance(1, 4);
+    let message = generateMessage(numbers, operation);
 
-    answer = game_handler.getAnswer(message);
-    correctAnswers = correctAnswers + game_handler.processAnswer(
+    answer = gameHandler.getAnswer(message);
+    correctAnswers += gameHandler.processAnswer(
       answer,
-      eval(message).toString(),
+      calculateAnswer(numbers, operation).toString(),
       name,
     );
     if (correctAnswers === i) {
       return;
     }
   }
-  game_handler.finishRound(name);
+  gameHandler.finishRound(name);
 }
