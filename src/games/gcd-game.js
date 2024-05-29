6946a80d23860a9367f3b9770549c3de43332ec9
TimeToCodeSomething <osel15278@gmail.com>
2024-05-29 16:50:01 +0700

    gcd-game.js style fix

--- src/games/gcd-game.js
+++ src/games/gcd-game.js
@@ -1,22 +1,21 @@
-import * as game_handler from '../index.js';
+import * as gameHandler from '../index.js';
 
 function gcd(number1, number2) {
   if (number2 === 0) {
     return number1;
-  } else {
-    return gcd(number2, number1 % number2);
   }
+  return gcd(number2, number1 % number2);
 }
 
 export default function gcdGame(name) {
   console.log('Find the greatest common divisor of given numbers.');
-  var correctAnswers = 0;
-  var answer = '';
-  var numbers = [];
-  for (var i = 0; i < 3; i++) {
-    numbers = game_handler.generateInstance(2, 100);
-    answer = game_handler.getAnswer(`${numbers[0].toString()} ${numbers[1].toString()}`);
-    correctAnswers = correctAnswers + game_handler.processAnswer(
+  let correctAnswers = 0;
+  let answer = '';
+  let numbers = [];
+  for (let i = 0; i < 3; i += 1) {
+    numbers = gameHandler.generateInstance(2, 100);
+    answer = gameHandler.getAnswer(`${numbers[0].toString()} ${numbers[1].toString()}`);
+    correctAnswers = correctAnswers + gameHandler.processAnswer(
       answer,
       gcd(numbers[0], numbers[1]).toString(),
       name,
@@ -25,5 +24,5 @@ export default function gcdGame(name) {
       return;
     }
   }
-  game_handler.finishRound(name);
+  gameHandler.finishRound(name);
 }
