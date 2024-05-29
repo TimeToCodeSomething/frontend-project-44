466288285cbe1e1ddadfeb9b3ff9354bb4969ba8
TimeToCodeSomething <osel15278@gmail.com>
2024-05-29 16:38:12 +0700

    calculator.js style fix

--- src/games/calculator.js
+++ src/games/calculator.js
@@ -1,24 +1,27 @@
 import * as gameHandler from '../index.js';
 
 function generateMessage(numbers, operation) {
-  let operations = {
+  const operations = {
     1: '+',
     2: '-',
     3: '*',
   };
-  let message = `${numbers[0]} ${operations[operation]} ${numbers[1]}`;
+  const message = `${numbers[0]} ${operations[operation]} ${numbers[1]}`;
 
   return message;
 }
 
-function calculateAnswer(numbers, operation){
+function calculateAnswer(numbers, operation) {
+  let correctAnswer = 0
   if (operation[0] === 1) {
-    return Number(numbers[0]) + Number(numbers[1]);
+   correctAnswer = Number(numbers[0]) + Number(numbers[1]);
   } else if (operation[0] === 2) {
-    return Number(numbers[0]) - Number(numbers[1]);
+    correctAnswer = Number(numbers[0]) - Number(numbers[1]);
   } else if (operation[0] === 3) {
-    return Number(numbers[0]) * Number(numbers[1]);
+    correctAnswer = Number(numbers[0]) * Number(numbers[1]);
   }
+
+  return correctAnswer;
 }
 
 export default function calculator(name) {
@@ -29,10 +32,10 @@ export default function calculator(name) {
   let answer = '';
   let correctAnswers = 0;
 
-  for (var i = 0; i < 3; i += 1) {
+  for (let i = 0; i < 3; i += 1) {
     numbers = gameHandler.generateInstance(2, 100);
     operation = gameHandler.generateInstance(1, 4);
-    let message = generateMessage(numbers, operation);
+    const message = generateMessage(numbers, operation);
 
     answer = gameHandler.getAnswer(message);
     correctAnswers += gameHandler.processAnswer(
